505208dcd22948000ac273d634924e25a9bc54a9
dude lao <brycelipscomb@gmail.com>
2021-08-06 17:54:50 -0400

    initial commit from CRA with some boilerplate app modifications

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import CharacterCreator from './components/CharacterCreator';
+
+function App() {
+  return (
+    <div className="App">
+      the app
+      <CharacterCreator />
+    </div>
+  );
+}
+
+export default App;
