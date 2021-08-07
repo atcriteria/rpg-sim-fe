af3df69c92bbfab7d8ef4c0ee83af466e0023059
dude lao <brycelipscomb@gmail.com>
2021-08-07 04:11:46 -0400

    added submitCharacter utility to help in submitting the character. This is mostly to reduce the clutter in the CharacterCreator and App components since I will likely be doing API calls here to keep players from hacking their characters, if I decide I want to make a back end for this

---
+++ src/util/submitCharacter.js
@@ -0,0 +1,19 @@
+// this is a helper function for submitting
+// a created character. When I scale the app
+// up a bit this function may have to do calls
+// and access more information that I don't want
+// cluttering up the main App file.
+
+// Takes in a character and if it is a valid character
+// then we stringify it and store it in local storage
+// and return true, otherwise we return false. In the
+// future this will likely make an API call so users
+// cannot easily hack their characters.
+export default function submitCharacter(character){
+    if(character){
+        window.localStorage.setItem("player", JSON.stringify(character))
+        return true
+    } else {
+        return false
+    }
+}
