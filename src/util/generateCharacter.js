0ee87523fabe8ff0f63b0d3cdcf3823105642201
dude lao <brycelipscomb@gmail.com>
2021-08-07 04:10:40 -0400

    created a generateCharacter helper to assist in compiling new characters with the baseline character stats. Will also help in the future if I want to let players choose a class of some sort that may have it's own buffs

---
+++ src/util/generateCharacter.js
@@ -0,0 +1,20 @@
+// A helper function for generating a character
+// by combining the submitting information with
+// the base character information. The base info
+// may change later and become more complex so
+// we're offloading this here
+
+export default function generateCharacter(character){
+    const newCharacter = {
+        name: character.name,
+        hp: 20 + character.hp,
+        sp: 5 + character.sp,
+        atk: 5 + character.atk,
+        def: 0 + character.def,
+        accuracy: 0 + character.accuracy,
+        "sp-atk": 0,
+        "sp-def": 0,
+        xp: 0,
+    }
+    return newCharacter
+}
