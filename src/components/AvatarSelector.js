8f447581b8667ae54ce80244a9acd227c2b9923e
dude lao <brycelipscomb@gmail.com>
2021-08-06 22:54:17 -0400

    refactoring how I want to do the avatar selector a bit

--- src/components/AvatarSelector.js
+++ src/components/AvatarSelector.js
@@ -2,23 +2,22 @@
 // creation that helps players cycle through
 // the base images
 
-import playerOne from "../images/players/player_1.png"
-import playerTwo from "../images/players/player_2.png"
-import playerThree from "../images/players/player_3.png"
-import playerFour from "../images/players/player_4.png"
-import playerFive from "../images/players/player_5.png"
-import playerSix from "../images/players/player_6.png"
-
+import {useState} from 'react';
+import playerAvatars from '../util/playerAvatars';
 
 export default function AvatarSelector({selectImage}){
+    const [state, setState] = useState(playerAvatars[0]);
+
+    const handleClick = e => {
+        e.preventDefault();
+        selectImage(state)
+    }
+
     return(
         <div>
-            <img className="avatar-selector one" onClick={selectImage} name={playerOne} src={playerOne} alt="Player Portrait" />
-            <img className="avatar-selector two" onClick={selectImage} name={playerTwo} src={playerTwo} alt="Player Portrait" />
-            <img className="avatar-selector three" onClick={selectImage} name={playerThree} src={playerThree} alt="Player Portrait" />
-            <img className="avatar-selector four" onClick={selectImage} name={playerFour} src={playerFour} alt="Player Portrait" />
-            <img className="avatar-selector five" onClick={selectImage} name={playerFive} src={playerFive} alt="Player Portrait" />
-            <img className="avatar-selector six" onClick={selectImage} name={playerSix} src={playerSix} alt="Player Portrait" />
+            <button onClick={handleClick} name="left" >{`<<`}</button>
+            <img className="avatar-selector" name={state} src={state} alt="Player Portrait" />
+            <button onClick={handleClick} name="right" >{`>>`}</button>
         </div>
     )
 }
---
+++ src/util/playerAvatars.js
@@ -0,0 +1,17 @@
+import one from "../images/players/player_1.png"
+import two from "../images/players/player_2.png"
+import three from "../images/players/player_3.png"
+import four from "../images/players/player_4.png"
+import five from "../images/players/player_5.png"
+import six from "../images/players/player_6.png"
+
+const playerAvatars = [
+    one,
+    two,
+    three,
+    four,
+    five,
+    six
+];
+
+export default playerAvatars;
