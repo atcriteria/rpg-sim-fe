d3c1ada1f90996b5d18d2c3685f05b85666bd6f0
dude lao <brycelipscomb@gmail.com>
2021-08-08 00:17:52 -0400

    refactoring some of the image content

---
+++ src/util/enemis.js
@@ -0,0 +1,16 @@
+// The master list of monsters
+// Organized by level group
+
+let weakMonsters = [
+    {
+        name: "monster",
+        img: "image/path/here",
+        hp: 20,
+        atk: 2,
+        def: 2,
+        "sp-atk": 0,
+        "sp-def": 0,
+        accuracy: 2,
+        xp: 20
+    }
+];
--- src/util/playerAvatars.js
+++ src/util/playerAvatars.js
@@ -1,9 +1,10 @@
-import one from "../images/players/player_1.png"
-import two from "../images/players/player_2.png"
-import three from "../images/players/player_3.png"
-import four from "../images/players/player_4.png"
-import five from "../images/players/player_5.png"
-import six from "../images/players/player_6.png"
+const origin = window.location.origin
+const one = `${origin}/images/players/player_1.png`
+const two = `${origin}/images/players/player_2.png`
+const three = `${origin}/images/players/player_3.png`
+const four = `${origin}/images/players/player_4.png`
+const five = `${origin}/images/players/player_5.png`
+const six = `${origin}/images/players/player_6.png`
 
 const playerAvatars = [
     one,
