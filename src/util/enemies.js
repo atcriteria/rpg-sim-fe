527d718ec76ee8496ea3823ae42d42c7bd729403
dude lao <brycelipscomb@gmail.com>
2021-08-10 17:03:40 -0400

    enemies*, starting up work again after a short break

---
+++ src/util/enemies.js
@@ -0,0 +1,38 @@
+// The master list of monsters
+// Organized by level group
+
+let weakMonsters = [
+    {
+        name: "monster",
+        img: "image/path/here",
+        hp: 20,
+        atk: 2,
+        def: 2,
+        "sp-atk": 0,
+        "sp-def": 0,
+        accuracy: 2,
+        xp: 20
+    },
+    {
+        name: "monster2",
+        img: "image/path/here",
+        hp: 20,
+        atk: 2,
+        def: 2,
+        "sp-atk": 0,
+        "sp-def": 0,
+        accuracy: 2,
+        xp: 20
+    },
+    {
+        name: "monster3",
+        img: "image/path/here",
+        hp: 20,
+        atk: 2,
+        def: 2,
+        "sp-atk": 0,
+        "sp-def": 0,
+        accuracy: 2,
+        xp: 20
+    },
+];
