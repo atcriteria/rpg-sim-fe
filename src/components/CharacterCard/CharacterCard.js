4303b3cc1e4988c3d2c09776f7a482e1c0b7bfe3
dude lao <brycelipscomb@gmail.com>
2021-08-14 03:02:12 -0400

    character card work

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -7,7 +7,12 @@ export default function CharacterCard({character}){
         <div className="character-card-wrapper">
             <h2>{character.name}</h2>
             <img src={character.img} alt={`${character.name}'s avatar`}/>
-            <h3>{character.hp} / {character.maxHP}</h3>
+            <h3>HP: {character.hp} / {character.maxHP}</h3>
+            {/* In case we pass a monster (they don't have SP, so don't show it) */}
+            {(character.sp) ? <h3>SP: {character.sp} / {character.maxSP}</h3> : ""}
+            <h3>{character.atk}</h3>
+            <h3>{character.def}</h3>
+
         </div>
     )
 }
