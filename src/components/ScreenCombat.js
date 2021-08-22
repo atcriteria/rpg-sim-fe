06e01bef7af495bd8d8af658a7c227a0780d2d5a
dude lao <brycelipscomb@gmail.com>
2021-08-22 03:01:14 -0400

    put a test button to log out found monsters

--- src/components/ScreenCombat.js
+++ src/components/ScreenCombat.js
@@ -3,12 +3,14 @@
 
 // import {useState} from 'react';
 import CharacterCard from './CharacterCard/CharacterCard';
+import monsterFinder from '../util/combat/monsterFinder';
 
 export default function ScreenCombat({player}){
 
     return(
         <div className="combat-screen-wrapper">
             <CharacterCard character={player} />
+            <button onClick={monsterFinder(player)}>Find Monster</button>
         </div>
     )
 }
