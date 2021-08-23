5bbb9aea6f03a11f6828c959088ca736548bfa88
dude lao <brycelipscomb@gmail.com>
2021-08-23 03:11:24 -0400

    added more monsters and refactored the code a bit to be more easily accessible

--- src/util/combat/enemies.js
+++ src/util/combat/enemies.js
@@ -9,9 +9,20 @@
 // ${origin}/images/monsters/monster-img
 const origin = window.location.origin
 
+export default function monsterList(level){
+    switch(level){
+        case (level<=5):
+            return weakMonsters
+        case (level<=10):
+            return medMonsters
+        default:
+            return weakMonsters
+    }
+};
+
 let weakMonsters = [
     {
-        name: "monster",
+        name: "Caterpillar",
         img: `${origin}/images/monsters/caterpillar.png`,
         level: 1,
         hp: 20,
@@ -24,7 +35,7 @@ let weakMonsters = [
         xp: 20
     },
     {
-        name: "monster2",
+        name: "Crab",
         img: `${origin}/images/monsters/crab.png`,
         level: 2,
         hp: 25,
@@ -37,7 +48,7 @@ let weakMonsters = [
         xp: 25
     },
     {
-        name: "monster3",
+        name: "Goo",
         img: `${origin}/images/monsters/goo.png`,
         level: 3,
         hp: 30,
@@ -51,11 +62,44 @@ let weakMonsters = [
     },
 ];
 
-export default function monsterList(level){
-    switch(level){
-        case (level<=10):
-            return weakMonsters
-        default:
-            return weakMonsters
-    }
-};
+let medMonsters = [
+    {
+        name: "Strange Goo",
+        img: `${origin}/images/monsters/goo_01.png`,
+        level: 4,
+        hp: 35,
+        maxHP: 35,
+        atk: 6,
+        def: 4,
+        "sp-atk": 0,
+        "sp-def": 0,
+        accuracy: 2,
+        xp: 35
+    },
+    {
+        name: "Angry Frog",
+        img: `${origin}/images/monsters/frog_08.png`,
+        level: 5,
+        hp: 40,
+        maxHP: 40,
+        atk: 8,
+        def: 5,
+        "sp-atk": 0,
+        "sp-def": 0,
+        accuracy: 2,
+        xp: 40
+    },
+    {
+        name: "Silly Demon",
+        img: `${origin}/images/monsters/demon_10.png`,
+        level: 6,
+        hp: 50,
+        maxHP: 50,
+        atk: 11,
+        def: 5,
+        "sp-atk": 0,
+        "sp-def": 0,
+        accuracy: 2,
+        xp: 45
+    },
+];
