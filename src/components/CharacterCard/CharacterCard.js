4c3fe671257cf58662d381a672727b51532484d5
dude lao <brycelipscomb@gmail.com>
2021-08-23 03:05:58 -0400

    updated character cards to account for image sizes and additional stat indicators

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -7,11 +7,12 @@ export default function CharacterCard({character}){
         <div className="character-card-wrapper">
             <h2>{character.name}</h2>
             <img src={character.img} alt={`${character.name}'s avatar`}/>
+            <h3>Level: {character.level}</h3>
             <h3>HP: {character.hp} / {character.maxHP}</h3>
             {/* In case we pass a monster (they don't have SP, so don't show it) */}
             {(character.sp) ? <h3>SP: {character.sp} / {character.maxSP}</h3> : ""}
-            <h3>{character.atk}</h3>
-            <h3>{character.def}</h3>
+            <h3>Attack: {character.atk}</h3>
+            <h3>Defense: {character.def}</h3>
 
         </div>
     )
