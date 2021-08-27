f153701f09277b95a1d3a4532b03817bba14d82f
dude lao <brycelipscomb@gmail.com>
2021-08-27 05:17:19 -0400

    removed state from CharacterCards and kept them grabbing props from props

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -1,21 +1,19 @@
 // Pass in a player or monster object
 // to render the data onto a card.
 import './CharacterCard.css';
-import { useState } from 'react';
 
 export default function CharacterCard({character}){
-    const [state, setState] = useState(character)
     
     return(
         <div className="character-card-wrapper">
-            <h2>{state.name}</h2>
-            <img src={state.img} alt={`${state.name}'s avatar`}/>
-            <h3>Level: {state.level}</h3>
-            <h3>HP: {state.hp} / {state.maxHP}</h3>
+            <h2>{character.name}</h2>
+            <img src={character.img} alt={`${character.name}'s avatar`}/>
+            <h3>Level: {character.level}</h3>
+            <h3>HP: {character.hp} / {character.maxHP}</h3>
             {/* In case we pass a monster (they don't have SP, so don't show it) */}
-            {(state.player) ? <h3>SP: {state.sp} / {state.maxSP}</h3> : ""}
-            <h3>Attack: {state.atk}</h3>
-            <h3>Defense: {state.def}</h3>
+            {(character.player) ? <h3>SP: {character.sp} / {character.maxSP}</h3> : ""}
+            <h3>Attack: {character.atk}</h3>
+            <h3>Defense: {character.def}</h3>
         </div>
     )
 }
