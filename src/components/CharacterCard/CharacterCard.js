664d66d965484f77c01b2618bea892f328fcd7fc
dude lao <brycelipscomb@gmail.com>
2021-08-30 17:54:54 -0400

    some styling of the character cards

--- src/components/CharacterCard/CharacterCard.js
+++ src/components/CharacterCard/CharacterCard.js
@@ -9,11 +9,21 @@ export default function CharacterCard({character}){
             <h2>{character.name}</h2>
             <img src={character.img} alt={`${character.name}'s avatar`}/>
             <h3>Level: {character.level}</h3>
-            <h3>HP: {character.hp} / {character.maxHP}</h3>
-            {/* In case we pass a monster (they don't have SP, so don't show it) */}
-            {(character.player) ? <h3>SP: {character.sp} / {character.maxSP}</h3> : ""}
-            <h3>Attack: {character.atk}</h3>
-            <h3>Defense: {character.def}</h3>
+            <span className="char-card-span">
+                <h3>❤️ {character.hp} / {character.maxHP}</h3>
+                {/* In case we pass a monster (they don't have SP, so don't show it) */}
+                {(character.player) ? <h3>🪄 {character.sp} / {character.maxSP}</h3> : ""}
+            </span>
+            <span className="char-card-span">
+                <h3>⚔️ {character.atk}</h3>
+                <h3>🛡️ {character.def}</h3>
+            </span>
+            <span className="char-card-span">
+                {(character.player) ?
+                    <h3>🔖 {character.xp} / {character.tnl}</h3> :
+                    <h3>🔖 {character.xp}</h3>
+                }
+            </span>
         </div>
     )
 }
