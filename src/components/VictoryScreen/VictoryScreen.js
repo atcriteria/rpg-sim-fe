ed9994cc95ce794b9c828599fff51536d00345de
dude lao <brycelipscomb@gmail.com>
2021-10-14 22:30:32 -0400

    VictoryScreen work -- now displays a message if the player leveled up as well as the name of the monster they defeated

--- src/components/VictoryScreen/VictoryScreen.js
+++ src/components/VictoryScreen/VictoryScreen.js
@@ -3,6 +3,7 @@ import XPCounter from './XPCounter';
 
 export default function VictoryScreen(props){
     const {setShowVictoryScreen, combatData} = props;
+    let didLevelUp = (combatData.p.xp === 0) ? true : false
     let playerXPBeforeAdding = Math.max(0,(combatData.p.xp - 25));
     let playerXP = combatData.p.xp;
 
@@ -15,9 +16,13 @@ export default function VictoryScreen(props){
         <div className="victory-screen-wrapper">
             <div className="victory-screen-content-wrapper">
                 <div className="dialogue-section">
-                    This is what you won
+                    <p>You successfully defeated the {combatData.mon.name}!</p>
                     <p>You are awarded {combatData.mon.xp}XP!</p>
-                    <p>You are <span id="victory-screen-xp-counter">{playerXPBeforeAdding}</span>/{combatData.p.tnl} from level {combatData.p.level+1}</p>
+                    {
+                        (didLevelUp) ? 
+                        <p>Congratulations, you leveled up! Welcome to experience level {combatData.p.level}</p> :
+                        <p>You are <span id="victory-screen-xp-counter">{playerXPBeforeAdding}</span>/{combatData.p.tnl} from level {combatData.p.level+1}</p>
+                    }
                 </div>
                 <button onClick={handleClick} >Continue</button>
             </div>
