17f245ea369f94299a0f5f018a9c3fe5d22a767a
dude lao <brycelipscomb@gmail.com>
2021-10-19 00:12:35 -0400

    added some admin tooling to the menu to create an object and log it to the console so I can debug the classes

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,8 @@
 // The main menu displayed in the
 // GameWindow component.
 import './Menu.css';
+import apple from '../../util/inventory/listOfObjects';
+import GameObject from '../../util/inventory/GameObject';
 
 export default function Menu({updateScreen}){
 
@@ -15,13 +17,19 @@ export default function Menu({updateScreen}){
         window.location.reload();
     }
 
+    const testObjectsButton = e => {
+        e.preventDefault();
+        let item = new GameObject(apple)
+        console.log(item)
+    }
+
     return(
         <div>
             <nav className="main-menu-nav">
                 <button className="menu-nav-button" name="combat" onClick={handleClick} >Battle</button>
                 <button className="menu-nav-button" name="inventory" onClick={handleClick} >Inventory</button>
                 <button className="menu-nav-button" >Button</button>
-                <button className="menu-nav-button" >Button</button>
+                <button className="menu-nav-button" onClick={testObjectsButton} >Test Objects</button>
                 <button className="menu-nav-button" name="delete-player" onClick={handleDeletePlayer} >Delete Player</button>
             </nav>
         </div>
