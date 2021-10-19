137525b0ed4811195b256724724a7c6e63a77948
dude lao <brycelipscomb@gmail.com>
2021-10-18 20:53:01 -0400

    added an admin tool to clear local storage data to restart the char creation process

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -9,6 +9,12 @@ export default function Menu({updateScreen}){
         updateScreen(e.target.name)
     }
 
+    const handleDeletePlayer = e => {
+        e.preventDefault();
+        window.localStorage.removeItem("player")
+        window.location.reload();
+    }
+
     return(
         <div>
             <nav className="main-menu-nav">
@@ -16,7 +22,7 @@ export default function Menu({updateScreen}){
                 <button className="menu-nav-button" name="inventory" onClick={handleClick} >Inventory</button>
                 <button className="menu-nav-button" >Button</button>
                 <button className="menu-nav-button" >Button</button>
-                <button className="menu-nav-button" >Button</button>
+                <button className="menu-nav-button" name="delete-player" onClick={handleDeletePlayer} >Delete Player</button>
             </nav>
         </div>
     )
