66cbc20e69bf22a3d6e4829ccf7fca5347d950e7
dude lao <brycelipscomb@gmail.com>
2021-10-19 06:08:44 -0400

    rendering some inventory data to the ScreenInventory component as a proof of concept

--- src/components/ScreenInventory/ScreenInventory.js
+++ src/components/ScreenInventory/ScreenInventory.js
@@ -3,10 +3,17 @@
 import './ScreenInventory.css';
 
 export default function ScreenInventory({player}){
+    let inv = player.getInventoryItems();
+
     return(
         <div>
             Inventory Screen
             <p className="monies-p" title={`you have ${player.monies} monies to spend on items`}>💸 {player.monies}</p>
+            {
+                inv.map((item, ind) => {
+                    return (<div key={item.id}>{ind+1} - Name: {item.props.name}, Value: {item.props.value}</div>)
+                })
+            }
         </div>
     )
 }
