238b2a3e658461c1ff26125c5eb0de3fd78f85f9
dude lao <brycelipscomb@gmail.com>
2021-10-18 20:46:35 -0400

    bug fix in XPCounter -- if the user closed the window too quickly before the setTimeout of the function completed then the container would return as null and crash the app. We get around this by just doing a check each time we grab the container to ensure it is not null and if it is null then we kill the component

--- src/components/VictoryScreen/XPCounter.js
+++ src/components/VictoryScreen/XPCounter.js
@@ -6,6 +6,10 @@ export default function XPCounter(props){
             for(let i = currentXP; i < maxCurrentXP; i++){
                 setTimeout(() => {
                     let xpContainer = document.getElementById("victory-screen-xp-counter")
+                    // Error handling in case we close the victory screen before finishing this function
+                    if (!xpContainer) {
+                        return null
+                    }
                     xpContainer.innerText = i+1
                 }, 1000)
             }
