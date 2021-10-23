b73b3b123756c354e32006a2cd264d15fbbde31d
dude lao <brycelipscomb@gmail.com>
2021-10-23 07:16:35 -0400

    First test for Character class, initializing a new Character

---
+++ src/util/characters.test.js
@@ -0,0 +1,20 @@
+import Character from "./characters";
+const mockCharacterData = {
+    name: "Caterpillar",
+    img: `${origin}/images/monsters/caterpillar.png`,
+    level: 1,
+    hp: 20,
+    maxHP: 20,
+    atk: 2,
+    def: 2,
+    "sp-atk": 0,
+    "sp-def": 0,
+    accuracy: 2,
+    xp: 20
+};
+let char = null;
+
+test('Create A Character', () => {
+    char = new Character(mockCharacterData);
+    expect(char).toBeInstanceOf(Character)
+})
