7ee34a80c30a3c66ff2a4c118c7ee86f3e7e96a9
dude lao <brycelipscomb@gmail.com>
2021-10-23 07:21:24 -0400

    test for Character gaining XP

--- src/util/characters.test.js
+++ src/util/characters.test.js
@@ -12,9 +12,15 @@ const mockCharacterData = {
     accuracy: 2,
     xp: 20
 };
+let monster = new Character(mockCharacterData);
 let char = null;
 
 test('Create A Character', () => {
     char = new Character(mockCharacterData);
-    expect(char).toBeInstanceOf(Character)
-})
+    expect(char).toBeInstanceOf(Character);
+});
+
+test('Gain Experience', () => {
+    char.gainXP(monster);
+    expect(char.xp).toBe(40);
+});
