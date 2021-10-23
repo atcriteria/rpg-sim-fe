4c12e356a2d971209c1b9d00334df8744ac70e5a
dude lao <brycelipscomb@gmail.com>
2021-10-23 07:00:58 -0400

    added test for implementing an inventory to the player object and ensuring it is the appropriate class

--- src/util/item-system/inventory.test.js
+++ src/util/item-system/inventory.test.js
@@ -1,7 +1,14 @@
 import Inventory from "./inventory";
 
 const mockInventory = new Inventory();
+const player = {}
 
 test('Initialize Inventory', () => {
-    expect(mockInventory).toHaveProperty("items", {})
-})
+    expect(mockInventory).toHaveProperty("items", {});
+});
+
+test('Create Inventory For Player', () => {
+    expect(player).not.toHaveProperty("inv");
+    player.inventory = new Inventory();
+    expect(player.inventory).toBeInstanceOf(Inventory);
+});
