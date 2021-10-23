c8f78a9d7e2a7e507896498c0035558a5c928a31
dude lao <brycelipscomb@gmail.com>
2021-10-23 07:04:31 -0400

    test for ensuring we can add an item to the player inventory with the inventory class

--- src/util/item-system/inventory.test.js
+++ src/util/item-system/inventory.test.js
@@ -2,6 +2,8 @@ import Inventory from "./inventory";
 
 const mockInventory = new Inventory();
 const player = {}
+const playerWithInventory = {inventory: new Inventory()}
+const testItem = {id: 1, name: "test-item"}
 
 test('Initialize Inventory', () => {
     expect(mockInventory).toHaveProperty("items", {});
@@ -12,3 +14,9 @@ test('Create Inventory For Player', () => {
     player.inventory = new Inventory();
     expect(player.inventory).toBeInstanceOf(Inventory);
 });
+
+test('Add Item To Player Inventory', () => {
+    let inv = playerWithInventory.inventory;
+    inv.addItemToInventory(playerWithInventory, testItem);
+    expect(inv.items[1]).toHaveProperty("name", "test-item")
+})
