e0e81fc0d5544904729ace0d21be1761da7bd5b3
dude lao <brycelipscomb@gmail.com>
2021-10-23 05:23:46 -0400

    added a comment to describe where .default is coming from and why there is a check

--- src/util/item-system/GameObject.js
+++ src/util/item-system/GameObject.js
@@ -15,6 +15,11 @@ import generateUniqueID from "../generateUniqueID";
 export default class GameObject{
     constructor(item){
         this.id = (item.id) ? item.id : generateUniqueID();
+        // When importing an object in real time it is wrapped
+        // in a parent object, and the properties are located
+        // in parentObject.default. If we are creating a 
+        // GameObject without dynamically importing, we just
+        // need to grab all of their props from the object itself
         this.props = (item.default) ? {...item.default} : {...item};
     }
 }
