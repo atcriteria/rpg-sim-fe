44b35982d57919dfd213c86a5580aa044cafffcc
dude lao <brycelipscomb@gmail.com>
2021-10-30 19:22:48 -0400

    added a cloth hat item for testing armors and body

---
+++ src/items/armors/head/cloth_hat.js
@@ -0,0 +1,10 @@
+const clothHat = {
+    name: "cloth hat",
+    type: "head",
+    image: "none",
+    description: "a simple cloth hat that offers basic protection",
+    ac: 1,
+    value: 10
+};
+
+export default clothHat;
