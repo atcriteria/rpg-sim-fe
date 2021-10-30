49d322991d5694d553afbdb4383300980522782f
dude lao <brycelipscomb@gmail.com>
2021-10-30 01:03:03 -0400

    updated test with new model, delete the head class. It will probably work best to just run every item through GameObject rather than trying to run them through both GameObject and child objects given that this is a web app and I don't think it will be best for performance to have to iterate over item properties multiple times when initializing them

--- src/util/equipment/body.test.js
+++ src/util/equipment/body.test.js
@@ -1,20 +1,16 @@
 import Body from './body';
+import Head from './head/head';
+import clothHat from '../../items/armors/head/cloth_hat';
 
-let mockBody = {
-    "head": null,
-    "torso": null,
-    "legs": null,
-    "hands": null,
-    "wrist": null,
-    "lhand": null,
-    "rhand": null,
-    "ring": null,
-    "amulet": null
-}
-
+let testHelmet = new Head(clothHat);
 let charBody = new Body({});
 
-test('intitialize body class with empty body constructor', ()=> {
+test('intitialize body class with empty body constructor', () => {
     expect(charBody["head"]).toBeNull();
     expect(charBody["torso"]).toBeNull();
 });
+
+test('Can wear a helmet', () => {
+    charBody.wear(testHelmet)
+    expect(charBody["head"]).toHaveProperty("type", "head");
+})
