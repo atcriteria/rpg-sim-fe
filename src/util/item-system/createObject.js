202f1174206e5abb8822ab85916cc650d07e541f
dude lao <brycelipscomb@gmail.com>
2021-10-29 22:52:30 -0400

    added part of test for armor creation and refactored createObject to not always assume the item has a .default

--- src/util/item-system/createObject.js
+++ src/util/item-system/createObject.js
@@ -7,10 +7,12 @@ export default async function createObject(path){
     }
     try {
         let obj = new GameObject(await import(`../../items/${path}`))
-        for (let key in obj.default){
-            obj[key] = obj.default[key]
+        if (obj.default){
+            for (let key in obj.default){
+                obj[key] = obj.default[key]
+            }
+            delete obj.default;
         }
-        delete obj.default;
         return obj
     }
     catch {
--- src/util/item-system/createObject.test.js
+++ src/util/item-system/createObject.test.js
@@ -13,6 +13,8 @@ test('Defaults To Negative Item', async () => {
 
 test('Item Created Is Expected Item', async () => {
     const item = await createObject('food/apple.js');
+    const headArmor = await createObject('armors/head/cloth_hat.js');
     expect(item).toHaveProperty('name', 'apple');
     expect(item).toHaveProperty('value', 2);
+    expect(headArmor).toHaveProperty('ac', 1);
 })
